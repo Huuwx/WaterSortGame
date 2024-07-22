e9b9851f51599e492f1773064d7977eeca725722
Huuwx <chumanhhuu2@gmail.com>
2024-07-22 22:41:14 +0700

    first cm

---
+++ Assets/Scripts/GlassBottleController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlassBottleController : MonoBehaviour
+{
+    private Animator animator;
+    bool picked = false;
+
+    public Button myButton;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        if (myButton != null)
+        {
+            myButton.onClick.AddListener(OnButtonClicked);
+        }
+    }
+
+    void OnButtonClicked()
+    {
+        if (!picked)
+        {
+            Picked();
+            picked = true;
+        }
+        else
+        {
+            UnPick();
+            picked = false;
+        }
+    }
+
+    public void Picked()
+    {
+
+        animator.SetTrigger("Picked");
+
+    }
+
+    public void UnPick()
+    {
+
+        animator.SetTrigger("UnPick");
+
+    }
+}
